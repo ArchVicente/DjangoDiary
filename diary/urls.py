6c0f88c5d6a330f8514017ed84bd14f29b4e0150
ArchVjescorpion <archlinuxvicente@gmail.com>
2025-06-08 12:50:34 +0200

    Unimos las VIEWS con diary, creamos los enlaces y finaliza funcionalidad incial

--- diary/urls.py
+++ diary/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('entries/', include('entries.urls')),  # Include the entries app URLs
 ]
